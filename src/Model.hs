d6c7d779d9b02c630067563809eaae333702a160
seta <seta@seta-lap.riviera.pw.edu.pl>
2012-05-16 00:37:35 +0200

    finish gui (zabezpieczenia wszystkie dzialaja)

---
+++ src/Model.hs
@@ -0,0 +1,163 @@
+module Model where
+
+import Char
+import Data.Time hiding (Day)
+
+---------------------------------------
+--data definitions---------------------
+---------------------------------------
+data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sun deriving (Show, Enum, Read)
+
+-- nazwa dni stacje
+data Train = Train String [Day] [Id]
+
+--konstruktor: St name [lista pociagow]
+data Station = Station String [Arrival]
+
+--konstruktor: Arr pociagId czasPrzyjazdu
+data Arrival = Arrival Id TimeOfDay TimeOfDay
+
+data (Named a) => DB a = DB [a]
+
+data DBS = DBS (DB Station) (DB Train)
+
+data When = When [Day] TimeOfDay
+
+data Id = Id String
+
+
+----------------------------------------
+--type classes definitions--------------
+----------------------------------------
+class Named d  where
+    getName :: d -> String
+
+
+class Database d where
+    --empty      :: d
+    getObjects :: (Named a) => (d a) -> [a]
+    setObjects :: (Named a) => [a] -> (d a) -> (d a)
+    insert     :: (Named a) =>[a] -> (d a) -> (d a)
+    insert st db = setObjects os' db where
+                 os' = st ++ os
+                 os =  getObjects db
+
+    findAllByName :: (Named a) => String -> (d a) -> [a]
+    findAllByName name db = filter (\it -> getName it == name) (getObjects db)
+
+    exists :: (Named a) => String -> (d a) -> Bool
+    exists name db = length (findAllByName name db) > 0
+
+    remove :: (Named a) => String -> (d a) -> (d a)
+    remove name db = setObjects newObj db where
+                 obj = getObjects db
+                 newObj = concat (map (\it -> if getName it == name then [] else [it]) obj)
+-----------------------------------------
+--class instance definitions-------------
+-----------------------------------------
+instance (Named a,Show a) => Show (DB a) where
+    show (DB x) = concat (map show x)
+
+instance Named Id where
+    getName (Id a) = a
+
+instance Named Train where
+    getName (Train name days stations) = name
+
+instance Named Arrival where
+    getName (Arrival tr time st) = getName tr
+
+instance Named Station where
+    getName (Station name arrs) = name
+
+instance Database DB where
+    getObjects (DB x) = x
+    setObjects a (DB x) = DB a
+
+instance Show Train where
+    show (Train name days stations) = ret where
+                    ret = "Pociag [" ++ name ++ "]\n" ++ join (names stations) " -> "  ++"\n"
+                    names a = map (\it -> getName it) a
+
+instance Show Station where
+    show (Station name arrs) = "Stacja [" ++ name ++ "]\n" ++ concat (map show arrs)
+
+instance Show Arrival where
+    show (Arrival tr timeIn timeOut) = show timeIn ++ " " ++ show timeOut ++ " " ++ getName tr ++ " -> "
+
+instance Eq Day where
+    c == c' = fromEnum c == fromEnum c'
+
+
+----------------------------------------
+--Helper Methods------------------------
+----------------------------------------
+
+printArrival :: String -> Arrival -> DBS -> String
+printArrival stName arr (DBS sdb tdb) = ret where
+    (Station _ arrs) = head (findAllByName stName sdb)
+    arrStr = map show arrs
+    ret = show arr ++ getNext stations stName
+    (Train _ _ stations) = head (findAllByName (getName arr) tdb)
+    getNext (x:[]) n = "Ostatnia stacja\n"
+    getNext (x:xs) n  | (n == getName x) = getName (head xs)
+                      | otherwise = getNext xs n
+
+
+
+
+--bierze nastepna stacje pociagu
+getNextStation :: String -> String -> DBS -> String
+getNextStation stName trName (DBS sdb tdb) = ret where
+    ret = getNext stations stName
+    (Train _ _ stations) = head (findAllByName trName tdb)
+    getNext (x:xs) n  | (n == getName x) = getName (head xs)
+                      | otherwise = getNext xs n
+
+
+join :: [String] -> String -> String
+join [] separator = ""
+join (x:[]) separator = x
+join (x:xs) separator = concat ( [x] ++ [separator] ++ [join xs separator])
+
+
+empty :: DBS
+empty = DBS (DB []) (DB [])
+
+insertArrivals :: Station -> [Arrival] -> Station
+insertArrivals (Station name arrs) arr = Station name (arr ++ arrs)
+
+replaceArrivals :: Station -> [Arrival] -> Station
+replaceArrivals (Station name arrs) arr = Station name arr
+
+renameStation :: Station -> [String] -> Station
+renameStation (Station _ arrs) names = (Station (head names) arrs)
+
+getArrivals :: Station -> [Arrival]
+getArrivals (Station name arrs) = arrs
+
+getDays :: Train -> [Day]
+getDays (Train _ days _) = days
+
+
+
+removeStationFromTrain :: String -> Train -> Train
+removeStationFromTrain stId (Train name days st) = (Train name days stations) where
+    stations = concat (map (\it -> if getName it == name then [] else [it]) st)
+
+
+
+getId :: (Named a) => a -> Id
+getId a = (Id (getName a))
+
+
+removeTrainArrival :: Station -> String -> Station
+removeTrainArrival (Station name arrs) trainName = (Station name arrs') where
+    arrs' = concat (
+                    map
+                    (\it ->
+                         if trainName == getName it
+                         then []
+                         else [it])
+                    arrs
+                   )
---
+++ src/main.hs
@@ -0,0 +1,37 @@
+import API
+import Model
+import System.Environment
+import Data.Time hiding (Day)
+
+main :: IO ()
+
+s2d :: String -> TimeOfDay
+s2d s = read s::TimeOfDay
+
+
+
+
+
+getTestData :: DBS
+getTestData = ret where
+    tdb =  DB [(Train "Mieszko" [Mon, Tue, Fri, Sat, Sun] []), (Train "Chrobry" [Mon, Tue, Fri, Sat, Sun] [])]
+    tdb2 = addTrain "Walesa" [Mon,Tue,Fri,Sat] tdb
+    sdb = DB [(Station "Warszawa" []),(Station "Krakow" []),(Station "Zakopane" [])]
+    sdb2 = addStation "Wroclaw" sdb
+    sdb3 = addStation "Bydgoszcz" sdb2
+    dbs1 = addStationToTrain "Warszawa" "Mieszko" (s2d "11:10:00") (s2d "11:15:00") (DBS sdb3 tdb2)
+    dbs2 = addStationToTrain "Krakow" "Mieszko" (s2d "14:40:00") (s2d "14:45:00") dbs1
+    dbs3 = addStationToTrain "Zakopane" "Mieszko" (s2d "16:40:00") (s2d "16:45:00") dbs2
+    dbs4 = addStationToTrain "Wroclaw" "Chrobry" (s2d "10:20:00") (s2d "10:22:00") dbs3
+    dbs5 = addStationToTrain "Krakow" "Chrobry" (s2d "14:42:00") (s2d "14:47:00") dbs4
+    dbs6 = addStationToTrain "Bydgoszcz" "Chrobry" (s2d "16:40:00") (s2d "16:45:00") dbs5
+    dbs7 = addStationToTrain "Wroclaw" "Walesa" (s2d "09:00:00") (s2d "09:04:00") dbs6
+    dbs8 = addStationToTrain "Warszawa" "Walesa" (s2d "11:00:00") (s2d "11:04:00") dbs7
+        
+    ret = dbs8
+
+main = ret where
+    x (DBS (DB a) _) = map show a
+    y (DBS _ (DB a)) = map show a
+    tdb (DBS _ a) = a
+    ret = do putStrLn (concat (x getTestData) ++ concat (y getTestData) ++ getTimetableForStation "Warszawa" Mon getTestData)
